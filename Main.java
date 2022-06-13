957ca6bb8741c7de5c532dcb64965682db1c5b04
m1m22a16-saegusa <m1m22a16@st.oit.ac.jp>
2022-06-13 10:51:27 +0900

    Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     Monster a = new Monster();
-<<<<<<< HEAD
     Player b = new Player();
     System.out.println("Hello SEMonster!");
-=======
-    System.out.println("SEMONSTER!!!");
->>>>>>> main
     System.out.println(a.toString());
     b.drawMonsters();
     b.showDeck();
